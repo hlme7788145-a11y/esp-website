47797a96782f1951bd0a35d5336e4efc6df22d70
حليم <285200070+hlme7788145-a11y@users.noreply.github.com>
2026-08-23 00:51:02 +0300

    Add files via upload

---
+++ IERC20Permit.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+// Draft IERC20Permit extension by OpenZeppelin
+pragma solidity ^0.8.0;
+
+import { IERC20 } from "../../interfaces/IERC20Permit.sol";
---
+++ Permitable.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+// Permitable extension for OpenZeppelin
+pragma solidity ^0.8.0;
+
+import { IERC20Permit } from "../../interfaces/IERC20Permit.sol";
